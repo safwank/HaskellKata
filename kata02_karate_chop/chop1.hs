bd3b3592c048d5a9b00f286fd9964676a9faf60d
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2015-04-26 21:38:34 +1000

    Fix bugs

--- kata02_karate_chop/chop1.hs
+++ kata02_karate_chop/chop1.hs
@@ -3,29 +3,26 @@ import Test.QuickCheck
 
 chop :: Int -> [Int] -> Int -- list should be Ordered
 chop _ [] = -1
-chop int [x]
-  | int == x = 0
+chop x [y]
+  | x == y = 0
   | otherwise = -1
-chop int list =
+chop x xs =
   let start = 0
-      end = (length list) - 1
-  in chop' int list start end
+      end = (length xs) - 1
+  in chop' x xs start end
 
 chop' :: Int -> [Int] -> Int -> Int -> Int
-chop' int list start end
-  | start == end = list !! start
-  | end - start == 1 = if int == (list !! start)
-                          then start
-                          else if int == (list !! end)
-                                  then end
-                          else -1
+chop' x xs start end
+  | start == end = if x == (xs !! start)
+                      then start
+                      else -1
   | otherwise =
       let mid = middle $ [start..end]
-          first = list !! mid
-          second = list !! (mid + 1)
-      in if int <= first
-        then chop' int list start mid
-        else chop' int list (mid + 1) end
+          first = xs !! mid
+          second = xs !! (mid + 1) in
+      if x <= first
+         then chop' x xs start mid
+         else chop' x xs (mid + 1) end
 
 middle :: [Int] -> Int
 middle []         = -1
@@ -39,4 +36,6 @@ fromJust Nothing = -1
 fromJust (Just x) = x
 
 prop_chop :: Int -> [Int] -> Bool
-prop_chop x xs = chop x (sort xs) == (fromJust $ elemIndex x (sort xs))
+prop_chop x xs =
+  let sorted_xs = sort xs
+  in chop x sorted_xs == (fromJust $ elemIndex x sorted_xs)
