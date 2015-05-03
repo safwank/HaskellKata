bac3120bc917f87da67ac75d80393fb5838aa8b4
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2015-05-03 18:02:06 +1000

    Improve readability of WeatherMunger

--- src/kata04-data-munging/WeatherMunger.hs
+++ src/kata04-data-munging/WeatherMunger.hs
@@ -8,7 +8,7 @@ import Data.Csv
 import Data.List
 import qualified Data.Vector as V
 
-data Weather = Weather { dy :: !Int, mxT :: !Int, mnT :: !Int }
+data Weather = Weather { day :: !Int, maxTemp :: !Int, minTemp :: !Int }
 
 instance FromNamedRecord Weather where
   parseNamedRecord r = Weather <$> r .: "dy" <*> r .: "mxT" <*> r .: "mnT"
@@ -17,20 +17,18 @@ main :: IO ()
 main = do
   csvData <- BL.readFile "weather.dat"
   case decodeByName csvData of
-    Left err     -> putStrLn err
-    Right (_, v) -> mapM_ putStrLn $ showWeathers $ sortBy sortByTempSpread $ V.toList v
+    Left err            -> putStrLn err
+    Right (_, weathers) -> mapM_ putStrLn $ showWeathers $ sortBy sortByTempSpread $ V.toList weathers
 
-sortByTempSpread a@(Weather _ _ _) b@(Weather _ _ _)
+sortByTempSpread :: Weather -> Weather -> Ordering
+sortByTempSpread weatherA weatherB
   | spreadA < spreadB = LT
   | spreadA > spreadB = GT
-  | otherwise         = compare (dy a) (dy b)
-  where spreadA = (mxT a) - (mnT a)
-        spreadB = (mxT b) - (mnT b)
+  | otherwise         = compare (day weatherA) (day weatherB)
+  where spreadA = (maxTemp weatherA) - (minTemp weatherA)
+        spreadB = (maxTemp weatherB) - (minTemp weatherB)
 
 showWeathers :: [Weather] -> [String]
 showWeathers weathers = map (\w ->
-  let day    = dy w
-      spread = (mxT w) - (mnT w)
-  in show day ++ " " ++ show spread) weathers
-
-
+  let spread = (maxTemp w) - (minTemp w)
+  in show (day w) ++ " " ++ show spread) weathers
