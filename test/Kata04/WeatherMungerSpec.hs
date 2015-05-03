adcc754e2b04b9eddaaba9619b7f7f9021d961dd
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2015-05-04 00:40:12 +1000

    Improve readability of WeatherMungerSpec

--- test/Kata04/WeatherMungerSpec.hs
+++ test/Kata04/WeatherMungerSpec.hs
@@ -6,38 +6,52 @@ import Kata04.WeatherMunger
 spec :: Spec
 spec = do
   describe "#compareSpreads" $ do
-    it "is EQ when the spreads are equal" $ do
+    context "when the spreads are equal" $ do
       let first = Weather 1 20 10
           second = Weather 1 20 10
-      compareSpreads first second `shouldBe` EQ
 
-    it "is LT when the first spread is smaller than the second" $ do
+      it "is EQ" $ do
+        compareSpreads first second `shouldBe` EQ
+
+    context "when the first spread is smaller than the second" $ do
       let first = Weather 1 20 15
           second = Weather 1 20 10
-      compareSpreads first second `shouldBe` LT
 
-    it "is GT when the first spread is bigger than the second" $ do
+      it "is LT" $ do
+        compareSpreads first second `shouldBe` LT
+
+    context "when the first spread is bigger than the second" $ do
       let first = Weather 1 20 10
           second = Weather 1 20 15
-      compareSpreads first second `shouldBe` GT
+
+      it "is GT" $ do
+        compareSpreads first second `shouldBe` GT
 
   describe "#calcSpread" $ do
-    it "is positive when the temperatures differ" $ do
+    context "when the temperatures differ" $ do
       let weather = Weather 1 23 12
-      calcSpread weather `shouldBe` 11
 
-    it "is zero when the temperatures are the same" $ do
+      it "is positive" $ do
+        calcSpread weather `shouldBe` 11
+
+    context "when the temperatures are the same" $ do
       let weather = Weather 1 23 23
-      calcSpread weather `shouldBe` 0
+
+      it "is zero" $ do
+        calcSpread weather `shouldBe` 0
 
   describe "#showWeathers" $ do
-    it "shows blank when there is no weather to display" $ do
-      showWeathers [] `shouldBe` []
+    context "when there is no weather to display" $ do
+      it "shows blank" $ do
+        showWeathers [] `shouldBe` []
 
-    it "shows day and spread for a single weather" $ do
-      showWeathers [Weather 1 20 15] `shouldBe` ["1 5"]
+    context "for a single weather" $ do
+      it "shows day and spread" $ do
+        showWeathers [Weather 1 20 15] `shouldBe` ["1 5"]
 
-    it "shows day and spread for multiple weathers" $ do
+    context "for multiple weathers" $ do
       let weatherA = Weather 1 20 15
           weatherB = Weather 2 25 20
-      showWeathers [weatherA, weatherB] `shouldBe` ["1 5", "2 5"]
+
+      it "shows day and spread" $ do
+        showWeathers [weatherA, weatherB] `shouldBe` ["1 5", "2 5"]
